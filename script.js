276b9ea4047b109d89250dc3eaec3ad50bbc3fd9
d-brady <d.brady0965@gmail.com>
2026-01-20 06:41:35 -0700

    numbered rows. Not grid squares

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-var div = document.createElement("div");
-document.getElementById("main");
-div.style.width = "16px";
-div.style.height = "16px";
-div.style.background = "white";
-div.style.border = "2px";
-div.style.color = "black";
+function makeGrids(size) {
+    let screen = document.querySelector(".sketch-screen");
+    for (let i = 0; i < size; i++) {
+        let column = document.createElement("div");
+        column.classList.add("column")
+        for (let j = 1; j <= size; j++) {
+            let row = document.createElement("div");
+            row.classList.add("row");
+            row.style.border = "2px solid black";
+            row.innerText = (i * size) + j;
+            column.appendChild(row);
+        }
+        screen.appendChild(column);
+    }
+}
+
+makeGrids(16);
 
 
 
-document.getElementById("main").appendChild(div);
 
 
-console.log(div);
+
+
