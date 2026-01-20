a450f40e6b8ac9a488301e3724487e065cea5f8e
d-brady <d.brady0965@gmail.com>
2026-01-20 05:50:34 -0700

    create 16x16 box dynamically with js

---
+++ script.js
@@ -0,0 +1,13 @@
+var div = document.createElement("div");
+div.style.width = "16px";
+div.style.height = "16px";
+div.style.background = "white";
+div.style.border = "2px";
+div.style.color = "black";
+
+
+
+document.getElementById("main").appendChild(div);
+
+
+console.log(div);
