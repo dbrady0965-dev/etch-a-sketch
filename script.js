466ce3dc5419a089985aea9fad1bebcd38e48d1f
d-brady <d.brady0965@gmail.com>
2026-01-20 07:04:31 -0700

    No changes. attempts to make full page grid sqaures still unsuccessful

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ function makeGrids(size) {
     for (let i = 0; i < size; i++) {
         let column = document.createElement("div");
         column.classList.add("column")
+        column.style.width = "16px";
         for (let j = 1; j <= size; j++) {
             let row = document.createElement("div");
             row.classList.add("row");
+            row.style.width = "16px";
             row.style.border = "2px solid black";
             row.innerText = (i * size) + j;
             column.appendChild(row);
